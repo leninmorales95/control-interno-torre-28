a3801620ae736867ee13f0528a5b8e58899869d8
leninmorales95 <morales95lenin@gmail.com>
2026-09-07 00:45:42 -0500

    Android native foundation: android-app/settings.gradle.kts

---
+++ android-app/settings.gradle.kts
@@ -0,0 +1,9 @@
+pluginManagement {
+    repositories { google(); mavenCentral(); gradlePluginPortal() }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories { google(); mavenCentral() }
+}
+rootProject.name = "Torre28Native"
+include(":app")
