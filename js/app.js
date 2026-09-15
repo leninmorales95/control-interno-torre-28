940335edf65215bd823d23c995f387cebabe55bc
Torre 28 Panel <leninmorales95@users.noreply.github.com>
2026-09-15 11:38:31 -0500

    Restaurar depositos fisicos del sotano 2

--- js/app.js
+++ js/app.js
@@ -1968,7 +1968,11 @@ panel.style.setProperty(
   23,22,21,20,19,18
 ]
 , izquierda: [24], derecha: [], acceso: 'Acceso a Sótano 2' },
-      S2: { nombre: 'Sótano 2', rango: 'Estacionamientos 25 al 41', arriba: [25,26,27,28,29,30,31,32,33], abajo: [40,39,38,37,36,35,34], izquierda: [41], derecha: [], acceso: 'Acceso a Sótano 3' },
+      S2: { nombre: 'Sótano 2', rango: 'Estacionamientos 25 al 41', arriba: [25,26,27,28,29,30,31,32,33], abajo: [
+        { tipo: 'deposito', titulo: 'Depósito Red Digital' },
+        { tipo: 'deposito', titulo: 'Depósito 2 Torre 28' },
+        40,39,38,37,36,35,34
+      ], izquierda: [41], derecha: [], acceso: 'Acceso a Sótano 3' },
       S3: { nombre: 'Sótano 3', rango: 'Estacionamientos 42 al 61', arriba: [42,43,44,45,46,47,48,49,50], abajo: [59,58,57,56,55,54,53,52,51], izquierda: [61,60], derecha: [], acceso: 'Acceso a Sótano 4' },
       S4: { nombre: 'Sótano 4', rango: 'Estacionamientos 62 al 81', arriba: [62,63,64,65,66,67,68,69,70], abajo: [79,78,77,76,75,74,73,72,71], izquierda: [81,80], derecha: [], acceso: 'Acceso a Sótano 5' },
       S5: { nombre: 'Sótano 5', rango: 'Estacionamientos 82 al 103', arriba: [82,83,84,85,86,87,88,89,90], abajo: [99,98,97,96,95,94,93,92,91], izquierda: [102,103], derecha: [101,100], acceso: '' }
